f49261b7e6066732e4ccf3e8638deb753b6a22b7
aakash2602-xi <aakashk2602@gmail.com>
2026-06-22 00:30:27 +0530

    Codeforces div4 1074 A

--- codeforces/div4_1074.cpp
+++ codeforces/div4_1074.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 typedef long long ll;
 
-//B
+//A - Perfect Root 
 int main(){
     ll t{0};
     cin >> t;
@@ -19,3 +19,21 @@ int main(){
     }
     return 0;
 }
+
+//B - Prefix Max 
+/*int main(){
+    ll t{0};
+    cin >> t;
+    while (t--) {
+        ll n{0},x{0};
+        cin >> n;
+        vector<ll> v(n,0);
+        for(ll i=0 ; i<n ; i++){
+            cin >> x;
+            v[i]=x;
+        }
+        sort(v.begin(),v.end());
+        cout << n*v[n-1]<<"\n";
+    }
+    return 0;
+}*/
