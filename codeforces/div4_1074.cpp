8a85c3c95914fb839a69507eec6893caadd74242
aakash2602-xi <aakashk2602@gmail.com>
2026-06-21 22:53:58 +0530

    Codeforces div4 1074 B

---
+++ codeforces/div4_1074.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+//B
+int main(){
+    ll t{0};
+    cin >> t;
+    while (t--) {
+        ll n{0},x{0};
+        cin >> n;
+        vector<ll> v(n,0);
+        for(ll i=0 ; i<n ; i++){
+            cin >> x;
+            v[i]=x;
+        }
+        sort(v.begin(),v.end());
+        cout << n*v[n-1]<<"\n";
+    }
+    return 0;
+}
