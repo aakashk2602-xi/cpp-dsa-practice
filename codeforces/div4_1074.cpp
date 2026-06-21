65a06f7dc6b8e9934611221d7d2615986aeee22e
aakash2602-xi <aakashk2602@gmail.com>
2026-06-22 00:31:58 +0530

    Codeforces div4 1074 C

--- codeforces/div4_1074.cpp
+++ codeforces/div4_1074.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long ll;
 
 //A - Perfect Root 
-int main(){
+/*int main(){
     ll t{0};
     cin >> t;
     while (t--) {
@@ -18,7 +18,7 @@ int main(){
         cout << n*v[n-1]<<"\n";
     }
     return 0;
-}
+}*/
 
 //B - Prefix Max 
 /*int main(){
@@ -37,3 +37,33 @@ int main(){
     }
     return 0;
 }*/
+
+//C - Shifted MEX 
+int main(){
+    ll t{0},count{0};
+    cin >> t;
+    while (t--) {
+        ++count;
+        ll n{0};
+        cin >> n;
+        vector<ll> v(n,0);
+        for(ll i=0 ; i<n ; i++){
+            ll y;
+            cin >> y;
+            v[i]=y;
+        }
+        sort(v.begin(),v.end());
+        ll x= -v[0];
+        set<ll> set;
+        for(ll i: v){
+            set.insert(i+x);
+        }
+        ll j=0;
+        for( auto i : set ){
+            if( i!=j ){break;}
+            else {++j;}
+        }
+        cout << j<<"\n";
+    }
+    return 0;
+}
