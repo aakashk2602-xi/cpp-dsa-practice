35780e6c58eacc2c53a2133e046c4f49553b7f03
aakash2602-xi <aakashk2602@gmail.com>
2026-06-23 23:18:27 +0530

    Codeforces div4 1062 B

--- codeforces/div4_1062.cpp
+++ codeforces/div4_1062.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long ll;
  
 //A - Square
-int main(){
+/*int main(){
     ll t{0};
     cin>> t;
     while (t--) {
@@ -15,4 +15,21 @@ int main(){
             cout << "NO\n";
         }
     }
+}*/
+
+//B - Your Name
+int main(){
+    ll test{0};
+    cin>> test;
+    while (test--) {
+        ll n{0};
+        cin>>n;
+        string s , t;
+        cin >> s >> t;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t){cout << "YES\n";}
+        else{cout << "NO\n";}
+    }
+    return 0;
 }
