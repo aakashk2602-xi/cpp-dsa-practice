cd639fd0a48ecdf837c7b23fb554c793a7cd32ab
aakash2602-xi <aakashk2602@gmail.com>
2026-06-25 23:06:20 +0530

    Codeforces div4 1050 B

--- codeforces/div4_1050.cpp
+++ codeforces/div4_1050.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long ll;
 
 //A. Sublime Sequence
-int main(){
+/*int main(){
     ll test{0};
     cin>> test;
     while (test--) {
@@ -14,6 +14,27 @@ int main(){
         }else{
             cout << 0 << "\n";
         }
+    }
+    return 0;
+}*/
+
+//B. Lasers
+int main(){
+    ll test{0};
+    cin>> test;
+    while (test--) {
+        ll n{0},m{0},x{0},y{0},count{0};
+        cin >> n >> m >> x >> y ;
+        vector<ll> a(n,0),b(m,0);
+        for( ll i{0} ; i < n ; i++ ){ cin >> a[i]; }
+        for( ll i{0} ; i < m ; i++ ){ cin >> b[i]; }
+        for( ll i{0} ; i < n ; i++ ){
+            if( a[i] <= y ){ ++count; }
+        }
+        for( ll i{0} ; i < m ; i++ ){
+            if( b[i] <= x ){ ++count; }
+        }
+        cout << count << "\n";
     }
     return 0;
 }
