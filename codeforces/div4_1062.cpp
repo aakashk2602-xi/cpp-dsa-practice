093d42a6123a0ded4cd17b5fd43d531a3a776dce
aakash2602-xi <aakashk2602@gmail.com>
2026-06-27 22:29:20 +0530

    Codeforces div4 1062 D

--- codeforces/div4_1062.cpp
+++ codeforces/div4_1062.cpp
@@ -53,3 +53,32 @@ typedef long long ll;
         for (int i = 0; i < n; ++i) {cout << a[i] << " \n"[i == n - 1];}
     }
 }*/
+
+//D. Yet Another Array Problem
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    using ll = long long;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<ll> a(n);
+        for (auto &i: a) cin >> i;
+        for (ll x : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53}) {
+            int ok = 0;
+            for (ll i : a) {
+                if (i % x) {
+                    ok = 1;
+                    break;
+                }
+            }
+            if (ok) {
+                cout << x << '\n';
+                break;
+            }
+        }
+    }
+}
