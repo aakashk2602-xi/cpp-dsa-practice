273af58868a1cc7c9400ea76011b415d3cec2b37
aakash2602-xi <aakashk2602@gmail.com>
2026-06-30 22:51:31 +0530

    Codeforces div3 1107 B(brute)

--- codeforces/div3_1107.cpp
+++ codeforces/div3_1107.cpp
@@ -24,3 +24,35 @@ int main(){
     }
 }
 */
+
+//B
+/*bool isGood(ll variable){
+    vector<ll> mp;
+    while (variable) {
+        mp.push_back(variable%10);
+        variable/=10;
+    }
+    std::sort(mp.begin(), mp.end());
+    mp.erase(std::unique(mp.begin(), mp.end()), mp.end());
+    if( mp.size() > 2 ) return false;
+    return true;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll x{0};
+        cin >> x ;
+        for( ll y = 2 ; y < INT_MAX ; y++ ){
+            if( isGood(y) && isGood(x*y)){
+                cout << y <<"\n";
+                break;
+            }
+        }
+    }
+}*/
+
