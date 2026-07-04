426e4874a9af25342646d77e90844930596e0a2c
aakash2602-xi <aakashk2602@gmail.com>
2026-07-05 01:20:48 +0530

    Codeforces div3 1107 D

--- codeforces/div3_1107.cpp
+++ codeforces/div3_1107.cpp
@@ -72,3 +72,29 @@ int main(){
 }*/
 
 //D
+int main(){
+ 
+    int tt;
+    cin >> tt;
+ 
+    while(tt--){
+        ll n;
+        cin >> n;
+        vector<ll> a(n), b(n);
+        for(ll i = 0; i < n; i++) cin >> a[i];
+        for(ll i = 0; i < n; i++) cin >> b[i];
+
+        for(ll i = 1; i < n; i++) {
+            a[i] += a[i - 1];
+            b[i] += b[i - 1];
+        }
+        
+        bool is = 1;
+        for(ll i = 0; i < n; i++) if(a[i] > b[i]) is = 0;
+ 
+        if(is) cout << "YES\n";
+        else cout << "NO\n";
+    }
+ 
+    return 0;
+}
