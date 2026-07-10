d9d71eb097ff4ec6a63317aa6bb8ea3be2556de8
aakash2602-xi <aakashk2602@gmail.com>
2026-07-11 03:39:19 +0530

    Codeforces div3 1096 C

--- codeforces/div3/div3_1096.cpp
+++ codeforces/div3/div3_1096.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <cstddef>
 using namespace std;
 typedef long long ll;
 
@@ -27,7 +28,7 @@ typedef long long ll;
 }*/
 
 // B - Party Monster
-int main(){
+/*int main(){
     ll t{0};
     cin >> t;
     while (t--) {
@@ -48,4 +49,31 @@ int main(){
         }
     }
     return 0;
-}
+}*/
+
+// C - Snowfall
+/*int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int> a, b, c, d;
+        for(int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            if(x % 6 == 0) a.push_back(x);
+            else if(x % 2 == 0) b.push_back(x);
+            else if(x % 3 == 0) c.push_back(x);
+            else d.push_back(x);
+        }
+
+        vector<int> ans;
+        for(auto it : a) ans.push_back(it);
+        for(auto it : b) ans.push_back(it);
+        for(auto it : d) ans.push_back(it);
+        for(auto it : c) ans.push_back(it);
+        
+        for(int i = 0; i < n; i++) cout << ans[i] <<  " \n"[i == n - 1];
+    }
+}*/
