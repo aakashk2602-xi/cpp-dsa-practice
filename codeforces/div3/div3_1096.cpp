62f1ada27c148064b171bb44c013f13c40057012
aakash2602-xi <aakashk2602@gmail.com>
2026-07-10 22:30:01 +0530

    Codeforces div3 1096 B

--- codeforces/div3/div3_1096.cpp
+++ codeforces/div3/div3_1096.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long ll;
 
 // A - Koshary
-int main(){
+/*int main(){
     ll t{0};
     cin >> t;
     while (t--) {
@@ -18,6 +18,30 @@ int main(){
         }
 
         if( x==0 and y==0 ){
+            cout << "YES\n";
+        }else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}*/
+
+// B - Party Monster
+int main(){
+    ll t{0};
+    cin >> t;
+    while (t--) {
+        ll n{0},open{0},close{0};
+        string s;
+        cin >> n >> s;
+        for( auto i : s ){
+            if( i == '(' ){
+                ++open;
+            }else {
+                ++close;
+            }
+        }
+        if(open==close){
             cout << "YES\n";
         }else {
             cout << "NO\n";
