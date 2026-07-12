31b327f043a15fdeb207bf0547e05b7fba561cf6
aakash2602-xi <aakashk2602@gmail.com>
2026-07-13 00:35:14 +0530

    Codeforces div2 1108 A(optimal)

--- codeforces/div2/div2_1108.cpp
+++ codeforces/div2/div2_1108.cpp
@@ -2,3 +2,20 @@
 using namespace std;
 typedef long long ll;
 
+void solve(){
+    ll n;
+    cin >> n;
+    for(ll i{0} ; i<n ; ++i)
+        cout << (i%2? i:i+2) <<" " ;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    ll _{0};
+    cin >> _ ;
+    while (_--) {
+        solve();
+        cout << "\n";
+    }
+}
