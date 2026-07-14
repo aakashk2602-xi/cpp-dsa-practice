de812037ddd8b8ead1c69247798ed98b8296d242
aakash2602-xi <aakashk2602@gmail.com>
2026-07-14 22:35:43 +0530

    Codeforces div3 1109 B

--- codeforces/div3/div3_1109.cpp
+++ codeforces/div3/div3_1109.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long ll;
 
 // A - Iskander and Drawings
-int main(){
+/*int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -21,4 +21,30 @@ int main(){
         }
         cout << ceil(double(hashcount)/2.0) << "\n";
     }
-}
+}*/
+
+// B - Nikita and Books
+/*int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    ll _{0};
+    cin >> _;
+    while (_--) {
+        ll n{0},sum{0};
+        cin >> n;
+        ll flag{1};
+        for(ll i{1} ; i <= n ; ++i){
+            ll x{0};
+            cin >> x;
+            sum += x;
+            if( (i*(i+1))/2 > sum ) flag = 0;
+        }
+        if(flag){
+            cout << "YES\n";
+        }else {
+            cout << "NO\n";
+        }
+ 
+    }
+}*/
