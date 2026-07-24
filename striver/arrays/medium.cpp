6facab9f1de05bae485aeb4307452e1cd3dc3170
aakash2602-xi <aakashk2602@gmail.com>
2026-07-24 23:57:31 +0530

    Striver DSA Sheet two sum

--- striver/arrays/medium.cpp
+++ striver/arrays/medium.cpp
@@ -3,60 +3,19 @@ using namespace std;
 typedef long long ll;
 
 // Two Sum
-
-class Solution {
-public:
-    // Variant 1: Check if two numbers sum to target using two-pointer approach
-    string twoSumExists(vector<int> arr, int target) {
-        int n = arr.size();
-        // Create a vector of pairs to remember original indices after sorting
-        vector<pair<int, int>> numsWithIndex;
-        for (int i = 0; i < n; i++) {
-            numsWithIndex.push_back({arr[i], i});
-        }
-        // Sort based on the element values
-        sort(numsWithIndex.begin(), numsWithIndex.end());
-
-        int left = 0, right = n - 1;
-        // Loop until pointers cross
-        while (left < right) {
-            int sum = numsWithIndex[left].first + numsWithIndex[right].first;
-            if (sum == target) {
-                return "YES";  // Pair found
-            } else if (sum < target) {
-                left++;  // Need bigger sum, move left pointer right
-            } else {
-                right--; // Need smaller sum, move right pointer left
+/* better solution --> O(NlogN) */
+/*vector<ll> solve(ll& size, ll *arr , ll& target){
+        map<ll , int32_t > hashmap;
+        ll rest{0};
+        for( ll i{0} ; i < size ;++i ){
+            rest = target - arr[i];
+            if(hashmap.find(rest)!=hashmap.end()){
+                return {hashmap[rest],i};
             }
+            hashmap[arr[i]] = i;
         }
-        return "NO";  // No pair found
-    }
-
-    // Variant 2: Return original indices of two numbers that sum to target
-    vector<int> twoSumIndices(vector<int> arr, int target) {
-        int n = arr.size();
-        vector<pair<int, int>> numsWithIndex;
-        for (int i = 0; i < n; i++) {
-            numsWithIndex.push_back({arr[i], i});
-        }
-        sort(numsWithIndex.begin(), numsWithIndex.end());
-
-        int left = 0, right = n - 1;
-        while (left < right) {
-            int sum = numsWithIndex[left].first + numsWithIndex[right].first;
-            if (sum == target) {
-                // Return original indices
-                return {numsWithIndex[left].second, numsWithIndex[right].second};
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-        return {-1, -1};  // No pair found
-    }
-};
-
+        return {-1,-1};
+}
 int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -64,14 +23,12 @@ int32_t main(){
     ll _{0};
     cin >> _;
     while (_--) {
-        ll n{0},k{0};
-        cin >> n >> k;
-        ll arr[n];
-        for(ll i{0} ; i < n ; ++i) cin >> arr[i];
-
-        for( ll i{0} ; i < n ;++i ){
-            ll rem = 0;
-        }
+        ll size{0},target{0};
+        cin >> size >> target;
+        ll arr[size];
+        for(ll i{0} ; i < size ; ++i) cin >> arr[i];
+        cout << solve(size,arr,target)[0] << " " << solve(size,arr,target)[1] << "\n";
     }
-}
+}*/
+/*optimal solution --> O()*/
 
