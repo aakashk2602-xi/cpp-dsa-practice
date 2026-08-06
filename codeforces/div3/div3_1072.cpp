12d751198c29c63c9678d6f3ebfedb0104663102
aakash2602-xi <aakashk2602@gmail.com>
2026-08-06 23:22:54 +0530

    Numerical method Lab Assignment

---
+++ codeforces/div3/div3_1072.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll _{0};
+    cin >> _;
+    while (_--) {
+        ll n{0};
+        cin >> n;
+        
+    }
+    return 0;
+}
