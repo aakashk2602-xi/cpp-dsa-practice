3ca5bef696b30d15b0b68c3366d1e3da41fb8053
aakash2602-xi <aakashk2602@gmail.com>
2026-08-09 23:46:38 +0530

    Codeforces div2 beta round 54 A

---
+++ codeforces/div2/div2_beta_round_54.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+// A - Chat room
+/*int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    string s,obj{"hello"};
+    cin >> s;
+    size_t i{0},j{0};
+    ll count{0};
+    while(i<s.size() && j<obj.size()){
+        if(obj[j]==s[i]){
+            ++j;
+            ++count;
+        }
+        ++i;
+    }
+    if(count==5) cout << "YES\n";
+    else cout << "NO\n";
+    return 0;
+}*/
+
--- codeforces/div3/div3_1072.cpp
+++ codeforces/div3/div3_1072.cpp
@@ -35,3 +35,16 @@ typedef long long ll;
     }
     return 0;
 }*/
+
+// C - Huge Pile
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    ll _{0};
+    cin >> _;
+    while (_--) {
+        
+    }
+    return 0;
+}
