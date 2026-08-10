7f574dc045cd42401d18262878007c0366d1dd0a
aakash2602-xi <aakashk2602@gmail.com>
2026-08-10 23:16:42 +0530

    Codeforces div2 1116 A

---
+++ codeforces/div2/div2_1116.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+// A - Three Numbers on the Blackboard
+/*int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll _{0};
+    cin >> _;
+    while (_--) {
+        vector<ll>v(3,0);
+        cin >>v[0]>>v[1]>>v[2];
+        sort(v.begin(),v.end());
+        if(v[0]+v[1]<v[2]) cout<<v[1];
+        else cout<<v[2]-v[0];
+        cout<<"\n";
+    }
+    return 0;
+}*/
