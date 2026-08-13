b35b5df0d6d69989c9b9a6acc4b3d3cdada94511
aakash2602-xi <aakashk2602@gmail.com>
2026-08-13 23:31:52 +0530

    NM Lab - 4 methods

--- codeforces/demo.cpp
+++ codeforces/demo.cpp
@@ -2,27 +2,112 @@
 using namespace std;
 typedef long long ll;
 
-bool isPrime(ll n) {
-    if (n <= 1) return false;
-    if (n == 2) return true;
-    if (n % 2 == 0) return false;
-    for (int i = 3; i * i <= n; i += 2) {
-        if (n % i == 0)
-            return false;
-    }
-    return true;
+//Bisection 
+/*inline double f(double x) {
+    return x*x*x-2*x*x-5;
 }
 
-int32_t main(){
-    ll _{0};
-    cin >> _;
-    while(_--){
-        ll n{0};
-        cin >> n;
-        if(n&1 && isPrime(n)){
-            cout << "YES\n";
-        }else{
-            cout << "NO\n";
-        }
+double solve(double a, double b, double tolerance) {
+    double m{0.0};
+    if(f(a)*f(b)>0) {
+        cout << "invalid interval\n";
+        return -1;
     }
+    if(f(a) == 0) return a;
+    if(f(b) == 0) return b;
+
+    while((b-a)/2.0>tolerance) {
+        m = (a+b)/2.0;
+        if(f(m)==0) break;
+        else if(f(a)*f(m)<0) b=m;
+        else a=m;
+    }
+    return (a+b)/2.0;
 }
+
+int32_t main() {
+    double a{0.0};
+    double b{3.0};
+    double tolerance{1e-4};
+    double root{solve(a,b,tolerance)};
+    cout << fixed << setprecision(4) << root << "\n";
+    return 0;
+}*/
+
+//Regula falsi
+/*inline double f(double x) {
+    return x*x-2;
+}
+double solve(double a, double b, double tolerance) {
+    double m{0.0};
+    if(f(a)*f(b)>0) {
+        cout<<"invalid interval\n";
+        return -1;
+    }
+    if(f(a)==0) return a;
+    if(f(b)==0) return b;
+    do{
+        m=(a*f(b)-b*f(a))/(f(b)-f(a));
+        if(f(m)==0) break;
+        else if(f(a)*f(m)<0) b=m;
+        else a=m;
+    }while(fabs(f(m))>tolerance);
+    return m;
+}
+int main() {
+    double a{0.0};
+    double b{2.0};
+    double tolerance{1e-6};
+
+    double root{solve(a,b,tolerance)};
+
+    cout<<fixed<<setprecision(6)<<root<<"\n";
+
+    return 0;
+}*/
+
+//Secant
+/*inline double f(double x) {
+    return x*x-2;
+}
+double solve(double x0, double x1, double tolerance) {
+    double f0{f(x0)}, f1{f(x1)};
+    double x2{x1};
+    while(fabs(x1-x0)>tolerance) {
+        x2=x1-f1*(x1-x0)/(f1-f0);
+        x0=x1;f0=f1;
+        x1=x2;f1=f(x1);
+    }
+    return x1;
+}
+int main() {
+    double x0{0.0};
+    double x1{2.0};
+    double tolerance{1e-6};\
+    double root{solve(x0, x1, tolerance)};
+    cout<<fixed<<setprecision(6)<<root<<"\n";
+    return 0;
+}*/
+
+//Newton raphson
+/*inline double f(double x) {
+    return x*x-2;
+}
+inline double fprime(double x) {
+    return 2*x;
+}
+double solve(double x0, double tolerance) {
+    double x1{x0};
+    do{
+        x0=x1;
+        x1=x0-f(x0)/fprime(x0);
+    }while(fabs(x1-x0)>tolerance);
+    return x1;
+}
+int main() {
+    double x0{1.0};
+    double tolerance{1e-6};
+    double root{solve(x0, tolerance)};
+    cout <<fixed<<setprecision(6)<<root<<"\n";
+    return 0;
+}*/
--- codeforces/div2/div2_1115.cpp
+++ codeforces/div2/div2_1115.cpp
@@ -2,49 +2,15 @@
 using namespace std;
 typedef long long ll;
 
+// A - Boss Fight
 int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     ll _{0};
-    cin >> _;
-    while (_--) {
-        ll n{0};
-        cin>>n;
-        vector<ll>v(n,0);
-        for(ll i{0} ; i<n;++i) cin>>v[i];
+    cin>>_;
+    while(_--){
 
-        map<ll,ll> hashmap;
-        for(auto i:v) ++hashmap[i];
-
-        vector<ll> ans(n,0);
-        auto map_n{hashmap.size()};
-        ll i{0},previous_element{0};
-        while(true){
-            ll count{0};
-            for(auto& [num,freq]:hashmap){
-                if(freq!=0){
-                    --freq;
-                    ans[i]=num;
-                    ++i;
-                }else {
-                    ++count;
-                }
-            }
-            if(count>=map_n-1){
-                count=0;
-                break;
-            }
-        }
-        for(auto& [num,freq]:hashmap){
-            if(freq!=0){
-                ans[i]=num;
-                break;
-            }
-        }
-        ll sum{0};
-        for(auto it:ans) sum+=it;
-        cout<<sum<<"\n";
     }
     return 0;
 }
