7337b299910db5eac18b84b236c8d9fb61e59d91
aakash2602-xi <aakashk2602@gmail.com>
2026-08-14 23:16:57 +0530

    Codeforces div2 1115 A(optimal)

--- codeforces/div2/div2_1115.cpp
+++ codeforces/div2/div2_1115.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -10,7 +11,17 @@ int32_t main(){
     ll _{0};
     cin>>_;
     while(_--){
-
+        ll n{0};
+        cin>>n;
+        vector<ll>v(n,0);
+        for(ll i{0};i<n;++i) cin>>v[i];
+        map<ll,ll> hashmap;
+        for(auto i:v) ++hashmap[i];
+        ll F{-1},X{-1},sum{0};
+        for(auto& [key,freq]:hashmap){
+            sum+=key*min(freq,n-freq+2);
+        }
+        cout<<sum<<"\n";
     }
     return 0;
 }
