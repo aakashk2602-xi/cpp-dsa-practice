f895b8b2604025cac28da84c844cc000cce45f04
aakash2602-xi <aakashk2602@gmail.com>
2026-08-15 23:23:06 +0530

    Codeforces div2 1115

--- codeforces/div2/div2_1115.cpp
+++ codeforces/div2/div2_1115.cpp
@@ -1,10 +1,9 @@
-#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
 // A - Boss Fight
-int32_t main(){
+/*int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -22,6 +21,19 @@ int32_t main(){
             sum+=key*min(freq,n-freq+2);
         }
         cout<<sum<<"\n";
+    }
+    return 0;
+}*/
+
+// B - Always Changing
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll _{0};
+    cin>>_;
+    while(_--){
+        
     }
     return 0;
 }
