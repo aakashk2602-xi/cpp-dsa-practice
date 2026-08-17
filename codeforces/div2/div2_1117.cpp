0742c97f5248814d9d8c5e8af3bc82f13bb27b02
aakash2602-xi <aakashk2602@gmail.com>
2026-08-17 22:41:45 +0530

    Codeforces div2 1117 B

--- codeforces/div2/div2_1117.cpp
+++ codeforces/div2/div2_1117.cpp
@@ -30,3 +30,34 @@ typedef long long ll;
     }
     return 0;
 }*/
+
+// B
+/*int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll _{0};
+    cin>>_;
+    while(_--){
+        ll n{0},m{0};
+        cin>>n>>m;
+        vector<ll> a(n,0),b(m,0);
+        for(ll i{0};i<n;i++) cin>>a[i];
+        for(ll i{0};i<m;i++) cin>>b[i];
+        ll sum1{n},sum2{m};
+        for(ll i{0};i<n-1;i++){
+            ll diff=a[i]-a[i+1];
+            sum1+=diff;
+        }
+        sum1+=a[n-1];
+        for(ll i{0};i<m-1;i++){
+            ll diff=b[i]-b[i+1];
+            sum2+=diff;
+        }
+        sum2+=b[m-1];
+        if(sum1>=sum2) cout<<1;
+        else cout<<2;
+        cout<<"\n";
+    }
+    return 0;
+}*/
