9983cc26bf95e0d9f1df3923c245087e565993eb
aakash2602-xi <aakashk2602@gmail.com>
2026-08-23 12:59:43 +0530

    Codeforces div1+div2 1104 A

---
+++ codeforces/div1+div2/div1_2/2_1104.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+// A
+/*int32_t main(){
+	ll _{0};
+	cin>>_;
+	while(_--){
+	    int32_t n{0},sum{0};cin>>n;
+		vector<ll> v(n,0);
+        ll x{INT_MAX};
+        for(int32_t i{0};i<n;++i){
+            cin>>v[i];
+            x=min(x,v[i]);
+            sum+=x;
+        }
+        
+        cout<<sum<<"\n";
+	}
+    return 0;
+}*/
+
+// B
