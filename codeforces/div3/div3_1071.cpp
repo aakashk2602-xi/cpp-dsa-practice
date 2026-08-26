8972c0ab2b3f658d0c2a620a24800b58e83c62b5
aakash2602-xi <aakashk2602@gmail.com>
2026-08-26 21:54:57 +0530

    Codeforces div3 1071 C

--- codeforces/div3/div3_1071.cpp
+++ codeforces/div3/div3_1071.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long ll;
 
 // A 
-int32_t main(){
+/*int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -15,4 +15,16 @@ int32_t main(){
         cout<<k*x+1<<"\n";
     }
     return 0;
-}
+}*/
+
+// C
+/*int32_t main(){
+    ll _{0};cin>>_;
+    while(_--){
+        ll n{0};cin>>n;
+        vector<ll> v(n,0);
+        for(ll i{0};i<n;i++) cin>>v[i];
+        sort(v.begin(),v.end());
+        cout<<max(v[0],v[1]-v[0])<<'\n';
+    }
+}*/
