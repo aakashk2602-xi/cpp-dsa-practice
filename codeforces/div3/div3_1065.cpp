559a1d366cc1e78dff6531f6fa03e54184d45c76
aakash2602-xi <aakashk2602@gmail.com>
2026-08-28 18:33:34 +0530

    Codeforces div3 1065 B

--- codeforces/div3/div3_1065.cpp
+++ codeforces/div3/div3_1065.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -18,6 +19,26 @@ int32_t main(){
     ll _{0};cin>>_;
     while(_--){
         ll n{0};cin>>n;
+        vector<ll> v(n,0);
+        ll find{0};
+        for(ll i{0};i<n;++i){
+            cin>>v[i];
+            if(i==0 or i==n-1 ){
+                find=max(find,v[i]);
+            }
+        }
+        for(ll i{0};i<n;++i){
+            if(v[i]==-1){
+                if(i!=0 && i!=n-1) v[i]=0;
+                else v[i]=find;
+            }
+        }
+        ll sum{0};
+        for(ll i{0};i<n-1;++i) sum+=(v[i+1]-v[i]);
+        cout<<abs(sum)<<"\n";
+        for(auto i:v) cout<<i<<" ";
+        cout<<"\n";
+
     }
     return 0;
 }
