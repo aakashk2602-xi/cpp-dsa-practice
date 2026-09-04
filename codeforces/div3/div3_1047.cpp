10f3e32a8df04bdd86295e0382571dc517e34aa2
aakash2602-xi <aakashk2602@gmail.com>
2026-09-04 12:43:45 +0530

    Codeforces div3 1047 A

---
+++ codeforces/div3/div3_1047.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+// A
+/*int32_t main(){
+    ll _{0};cin>>_;
+    while (_--) {
+        ll k{0},x{0};cin>>k>>x;
+        cout<<pow(2, k-1)*x<<"\n";
+    }
+    return 0;
+}*/
+
+// B
