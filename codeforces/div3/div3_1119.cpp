c1596ee684ba534d773ecba5c2ca1517dfe4ca15
aakash2602-xi <aakashk2602@gmail.com>
2026-09-05 23:32:54 +0530

    Codeforces div3 1119 B

--- codeforces/div3/div3_1119.cpp
+++ codeforces/div3/div3_1119.cpp
@@ -22,3 +22,25 @@ typedef long long ll;
 }*/
 
 // B - Minus Two
+/*int32_t main(){
+    ll _{0};cin>>_;
+    while(_--){
+        ll n{0};
+        cin>>n;
+        vector<ll> v(n,0);
+        for(ll i{0};i<n;++i) cin>>v[i];
+        ll a{0},b{0},c{0};
+        for(ll i{0};i<n;++i){
+            if(v[i]%2!=0){
+                ++a;
+            }else if(v[i]%2==0 && (v[i]/2)%2==0){
+                ++b;
+            }else{
+                ++c;
+            }
+        }
+        
+        cout<<max(a,max(b,c))<<endl;
+    }
+    return 0;
+}*/
