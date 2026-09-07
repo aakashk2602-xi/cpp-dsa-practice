f723c88a0c9a0876af84187694d7fc1a0b3d4c64
aakash2602-xi <aakashk2602@gmail.com>
2026-09-07 20:21:20 +0530

    Codeforces div2 1118 A

---
+++ codeforces/div2/div2_1118.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sort(x) sort(all(x))
+#define rsort(x) sort(all(x), [](int a, int b) { return a > b; })
+
+// A - Odd Eraser
+/*int32_t main(){
+    ll _{0};cin>>_;
+    while(_--){
+        ll n{0};cin>>n;
+        vector<ll> a(n,0);
+        for(ll i{0};i<n;++i) cin>>a[i];
+        cout<<gcd(a[0],a[n-1])<<"\n";
+    }
+}*/
+
+// B - Carrot Chopdown (Easy Version)
