67aca0d7c182717406f114f1cc0383c77dd36d71
aakash2602-xi <aakashk2602@gmail.com>
2026-09-08 22:55:54 +0530

    Codeforces Educational Round div2 194 C(brute force)

--- codeforces/div2/div2_educational_round_194.cpp
+++ codeforces/div2/div2_educational_round_194.cpp
@@ -42,3 +42,42 @@ typedef long long ll;
         cout<<ans<<'\n';
     }
 }*/
+
+// C - Maximize XOR, Minimize Operations
+/*int32_t main(){
+    ll _{0};cin>>_;
+    while(_--){
+        ll x{0},y{0};
+        cin>>x>>y;
+        ll copyx{x},copyy{y};
+        ll ans{-1};
+        ll count{-1};
+        if(x>y){
+            while(x!=y){
+                ans=max(ans,(x^y));
+                --x;++y;
+            }
+            ans=max(ans,x+y);
+            while(copyx>=0){
+                ++count;
+                if(ans==(copyx^copyy)) {--count;break;}
+                --copyx;++copyy;
+            }
+            if(ans==copyx+copyy) ++count;
+        }else{
+            while(x>0){
+                ans=max(ans,(x^y));
+                --x;++y;
+            }
+            ans=max(ans,x+y);
+            while(copyx>=0){
+                ++count;
+                if(ans==(copyx^copyy)) {--count;break;}
+                --copyx;++copyy;
+            }
+            if(ans==copyx+copyy) ++count;
+        }
+        cout<<ans<<" "<<count<<"\n";
+    }
+    return 0;
+}*/
