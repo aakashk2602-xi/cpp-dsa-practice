8c9966381507c62fed6c091e5debb81712bfd8e3
aakash2602-xi <aakashk2602@gmail.com>
2026-09-13 23:14:19 +0530

    Codeforces div2 1121 A

---
+++ codeforces/div2/div2_1121.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sort(x) sort(all(x))
+#define rsort(x) sort(all(x), [](int a, int b) { return a > b; })
+
+// A
+/*int32_t main(){
+    ll _{0};cin>>_;
+    while(_--){
+        ll n{0};cin>>n;
+        vector<ll> arr(n,0);
+        for(ll i{0};i<n;i++) cin>> arr[i];
+
+        vector<ll> newarr;
+        for(ll i{0};i<n;i++){
+            if(arr[i]!=i+1) newarr.push_back(arr[i]);
+        }
+        if(newarr.empty()){
+            cout<<"YES"<<"\n";
+            continue;
+        }
+        bool flag{true};
+        for(ll i=1;i<newarr.size();i++){
+            if(newarr[i-1]<newarr[i]){
+                flag=false;
+                break;
+            }
+        }
+        if(flag) cout<<"YES"<<"\n";
+        else cout<<"NO"<<"\n";
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}*/
+
+// B
