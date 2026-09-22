f13f678351eec2510314fdabf475ef839184f73f
aakash2602-xi <aakashk2602@gmail.com>
2026-09-22 10:56:15 +0530

    Codeforces div3 1122 A

---
+++ codeforces/div3/div3_1122.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define INF 1000000000000000000LL
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sort(x) sort(all(x))
+#define rsort(x) sort(all(x), [](int a, int b) { return a > b; })
+
+// A - Good Contest
+/*int32_t main(){
+    ll _{0};cin>>_;
+    while(_--){
+        ll n{0};cin>>n;
+        ll a{0},b{0},c{0};cin>>a>>b>>c;
+
+        cout<<n-min(a,min(b,c))<<"\n";
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}*/
+
