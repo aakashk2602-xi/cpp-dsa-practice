4f58eca73809abffea558dd72d53f97f94cb965d
aakash2602-xi <aakashk2602@gmail.com>
2026-09-25 23:22:07 +0530

    Codeforces div2 1123 A

---
+++ codeforces/div2/div2_1123.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define INF 1000000000000000000LL
+#define all(x) x.begin(), x.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sort(x) sort(all(x))
+#define rsort(x) sort(all(x), [](int a, int b) { return a > b; })
+
+// A - Turn Into a Palindrome  
+/*int32_t main(){
+    ll _{0};cin>>_;
+    while(_--){
+        ll n{0};cin>>n;
+        char c;cin>>c;
+        string s;cin>>s;
+        string rev_s(s.rbegin(),s.rend());
+        ll count{0};
+        for(ll i{0};i<n/2;i++){
+            if(s[i]!=rev_s[i]){
+                if(s[i]!=c and rev_s[i]!=c) count+=2;
+                if(s[i]==c) count++;
+                if(rev_s[i]==c) count++;
+            }
+        }
+        cout<<count<<"\n";
+
+        
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}*/
